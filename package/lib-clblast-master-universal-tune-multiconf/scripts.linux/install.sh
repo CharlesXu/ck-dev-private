dabd5b0b79beb1dcc44110973dad954bf9d3f996
Marco <mcian86@gmail.com>
2017-06-19 21:30:37 +0000

    Added package lib-clblast-master-universal-tune-ml

---
+++ package/lib-clblast-master-universal-tune-multiconf/scripts.linux/install.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+#
+# Extra installation script.
+#
+# See CK LICENSE.txt for licensing details.
+# See CK COPYRIGHT.txt for copyright details.
+#
+# Developer(s):
+# - Grigori Fursin, grigori.fursin@cTuning.org, 2017
+#
+
+export CK_CMAKE_EXTRA="${CK_CMAKE_EXTRA} \
+ -DOPENCL_LIBRARIES=${CK_ENV_LIB_OPENCL_LIB}/libOpenCL.so \
+ -DTUNERS=ON \
+ -DCLTUNE_ROOT:PATH=${CK_ENV_TOOL_CLTUNE} \
+ -DSAMPLES=ON \
+ -DTEST=ON \
+ -DCLIENTS=ON"
+
+return 0
